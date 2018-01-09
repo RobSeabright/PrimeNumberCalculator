598add0e03d4621326ae413e16648dee9cd81cb1
RobSeabright <robseabright@gmail.com>
2018-01-09 15:07:07 -0330

    Made things better.

--- src/PrimeNumberCalculator.java
+++ src/PrimeNumberCalculator.java
@@ -2,55 +2,36 @@
  * Created by 20133557 on 1/4/2018.
  */
 
+import org.jetbrains.annotations.NotNull;
+
 import java.lang.*;
 
 public class PrimeNumberCalculator {
 
-    public static StringBuilder primeFactors = new StringBuilder("");
-
     /**
-     *
-     * @param i positive integer to be factored
+     * @param n positive integer to be factored
      */
-    private static void getPrimeFactors(int i){
+    @NotNull
+    public static String getPrimeFactors(int n) {
 
-        while(i%2==0){
+        StringBuilder primeFactors = new StringBuilder("");
+
+        while (n % 2 == 0) {
             primeFactors.append("2*");
-            i /= 2;
+            n /= 2;
         }
 
-        for(int j=3; j<=Math.sqrt(i); j+=2){
-            while(i%j==0){
-                primeFactors.append(j).append("*");
-                i /= j;
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            while (n % i == 0) {
+                primeFactors.append(i).append("*");
+                n /= i;
             }
         }
-        if(i > 2){
-            primeFactors.append(i).append("*");
+        if (n > 2) {
+            primeFactors.append(n).append("*");
         }
-    }
 
-    /**
-     *
-     * @param i positive integer i
-     * @return primeFactors StringBuilder containing the prime factors of i
-     */
-    public static StringBuilder getUniquePrimeRepresentation(int i){
-        primeFactors.setLength(0);
-        getPrimeFactors(i);
-        primeFactors.setLength(primeFactors.length()-1);
-        return primeFactors;
+        primeFactors.setLength(primeFactors.length() - 1);
+        return primeFactors.toString();
     }
 }
-
-//    Used to find if a number is prime or not - legacy
-
-//    public static String isPrime(int i){
-//
-//        if(i%2==0) return i==2 ? "true" : "false";
-//
-//        for(int j=3; j<=Math.sqrt(i); j+=2){
-//            if(i%j==0) return "false";
-//        }
-//        return "true";
-//    }
--- src/PrimePrinter.java
+++ src/PrimePrinter.java
@@ -2,23 +2,34 @@
  * Created by 20133557 on 1/4/2018.
  */
 
+import java.util.Scanner;
+
 public class PrimePrinter {
 
     public static void main(String[] args) {
-        System.out.println("Prime Number Factorial Calculator");
-        System.out.println("Input Number | Prime Factors");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Prime Number Factorial Calculator\n");
+        System.out.println("Please enter a number greater than 1:");
+        while(!scanner.hasNextInt()) {
+            System.out.println("Please enter a number greater than 1:");
+            scanner.nextLine();
+        }
+        int upperLimit = scanner.nextInt();
+        System.out.println("\nInput Number | Prime Factors");
         System.out.println("-----------------------------");
-        printPrime(33);
+        printPrime(upperLimit);
     }
 
     /**
      * Prints the prime factors for each integer between 2 and upperLimit
      * @param upperLimit positive integer set by user
      */
-    public static void printPrime(int upperLimit){
+    private static void printPrime(int upperLimit){
         for (int i=2; i<=upperLimit; i++){
             System.out.printf("     %-8d|     %s\n", i,
-                        PrimeNumberCalculator.getUniquePrimeRepresentation(i));
+                        PrimeNumberCalculator.getPrimeFactors(i));
         }
     }
 }
